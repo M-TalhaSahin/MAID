8015c50ce0c8e8bdf3de0693872fd69675da40b9
Muhammed Talha Şahin <muhammedtalhasahin.tr@gmail.com>
2022-02-09 22:08:34 +0300

    initial

---
+++ MAID/PDPSApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MAID
+{
+    public partial class PDPSApp : Form
+    {
+        public PDPSApp()
+        {
+            InitializeComponent();
+        }
+
+        private void PDPSApp_Load(object sender, EventArgs e)
+        {
+            var dbconnection = new dataBase();
+            //dbconnection.clearDB();
+            //dbconnection.insertMaid("m1", "ms1");
+            //dbconnection.insertTemizlik(2, dataBase.odaTipi.cikis);
+            //dbconnection.removeMaid(1);
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ MAID/dataBase.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace MAID
+{
+    class dataBase
+    {
+        // Obtain connection string information from the portal
+        //
+        private static string Host = "localhost";
+        private static string User = "postgres";
+        private static string DBname = "PDPS";
+        private static string Password = "4458771";
+        private static string Port = "5432";
+        private static NpgsqlConnection connection;
+
+        public enum odaTipi
+        {
+            cikis = 0,
+            bakim = 1
+        }
+
+        public dataBase()
+        {
+            string connString =
+                   String.Format(
+                       "Server={0};Port={1};Database={2};User Id={3};Password={4}",
+                       Host,
+                       Port,
+                       DBname,
+                       User,
+                       Password
+                       );
+
+            connection = new NpgsqlConnection(connString);
+            connection.Open();
+        }
+        public void clearDB()
+        {
+            var command = new NpgsqlCommand(String.Format("delete from tblmaid"), connection);
+            command.ExecuteNonQuery();
+            command = new NpgsqlCommand(String.Format("delete from tbltemizlikkayit"), connection);
+            command.ExecuteNonQuery();
+        }
+        public void insertMaid(string name, string surname)
+        {
+            var command = new NpgsqlCommand(String.Format("insert into tblmaid(name, surname) values('{0}', '{1}')", name, surname), connection);
+            command.ExecuteNonQuery();
+            Console.Out.WriteLine("\nQuery executed.\n");
+        }
+        public void removeMaid(int ID)
+        {
+            var command = new NpgsqlCommand(String.Format("delete from tblmaid where maid_id = {0}", ID), connection);
+            command.ExecuteNonQuery();
+            Console.Out.WriteLine("\nQuery executed.\n");
+        }
+        public void insertTemizlik(int maidID, odaTipi odaTipi)
+        {
+            var command = new NpgsqlCommand(String.Format("insert into tbltemizlikkayit(maid_id, odatipi, date) values({0}, CAST({1} AS bit), NOW())", maidID, (int)odaTipi), connection);
+            command.ExecuteNonQuery();
+            Console.Out.WriteLine("\nQuery executed.\n");
+        }
+    }
+}
